f9672ffe4e62d36f97501239388c2dc1a991747c
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2026-05-08 01:10:52 +0900

    Create IScopeServicesExtentions.cs
    fix #33

---
+++ InteractionFlow.Standard/Builders/IScopeServicesExtentions.cs
@@ -0,0 +1,31 @@
+using InteractionFlow.Core.Builders;
+using InteractionFlow.Core.Entities.Architectures;
+using InteractionFlow.Core.Focuses;
+using InteractionFlow.Core.Interactions;
+
+namespace InteractionFlow.Standard.Builders
+{
+    public static class IScopeServicesExtentions
+    {
+        public static IScopeServices UseInteraction<TImplementation>(this IScopeServices @this)
+            where TImplementation : class, IInteraction
+        {
+
+            return @this.Use<TImplementation>();
+        }
+
+        public static IScopeServices UseFocus<TImplementation>(this IScopeServices @this)
+            where TImplementation : class, IFocus
+        {
+
+            return @this.Use<TImplementation>();
+        }
+
+        public static IScopeServices UseFunction<TService, TImplementation>(this IScopeServices @this)
+            where TService : class, IFlowNodeStateful
+            where TImplementation : class, TService
+        {
+            return @this.Use<TService, TImplementation>();
+        }
+    }
+}

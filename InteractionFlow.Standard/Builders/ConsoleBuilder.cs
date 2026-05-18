77e8c0ecb70d3cadd176419e2001bf88ebca0fa0
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2026-05-18 13:59:05 +0900

    Update ConsoleBuilder.cs
    - IConsoleColorAccess/IConsoleCursorPositionAccess の追加
    - 今後の拡張の可能性を考慮して sealed を撤廃
    - useCancellation フラグの定義

--- InteractionFlow.Standard/Builders/ConsoleBuilder.cs
+++ InteractionFlow.Standard/Builders/ConsoleBuilder.cs
@@ -4,24 +4,38 @@ using InteractionFlow.Standard.OperationPorts;
 using InteractionFlow.Standard.Operations;
 using InteractionFlow.Standard.ReactionPorts;
 using InteractionFlow.Standard.Reactions;
+using InteractionFlow.Standard.SilentExternalPorts;
+using InteractionFlow.Standard.SilentExternals;
 using System;
 
 namespace InteractionFlow.Standard.Builders
 {
-    public sealed class ConsoleBuilder : IScopeProfile
+    public class ConsoleBuilder : IScopeProfile
     {
-        public static IScopeProfile Profile { get; } = new ConsoleBuilder();
+        private readonly bool useCancellation;
 
-        private ConsoleBuilder() { }
+        public static IScopeProfile Profile { get; } = new ConsoleBuilder(false);
+        public static IScopeProfile ProfileUseCancellation { get; } = new ConsoleBuilder(true);
+
+        protected ConsoleBuilder(bool useCancellation)
+        {
+            this.useCancellation = useCancellation;
+        }
 
         public void Configure(IScopeServices builder)
         {
             builder.UseFunction<IConsoleOperation, ConsoleOperation>();
             builder.UseTransient<IConsoleOperation.IDummy, ConsoleOperation.Dummy>();
-
             builder.UseFunction<IConsoleWriter, ConsoleWriter>();
             builder.UseFunction<IExceptionPort<Exception>, ConsoleExceptionHandling>();
             builder.UseFunction<ICancellationPort, ConsoleCancellationHandling>();
+            builder.UseFunction<IConsoleColorAccess, ConsoleColorAccess>();
+            builder.UseFunction<IConsoleCursorPositionAccess, ConsoleCursorPositionAccess>();
+
+            if (useCancellation)
+            {
+                builder.UseFunction<ICancellationWithConsole, CancellationWithConsole>();
+            }
         }
     }
 }

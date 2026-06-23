e5cea29b8b2ad9c3a2c45b588ca13f5992e5da79
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2026-06-23 12:11:21 +0900

    Fix namespaces

--- InteractionFlow.Standard/Externals/SilentExternals/SilentSend.cs
+++ InteractionFlow.Standard/Externals/SilentExternals/SilentSend.cs
@@ -1,10 +1,10 @@
 using InteractionFlow.Core.Entities.Architectures;
 using InteractionFlow.Core.Entities.Contexts;
 using InteractionFlow.Core.Externals.SilentExternals;
-using InteractionFlow.Standard.ExternalPorts.SilentPorts;
+using InteractionFlow.Standard.ExternalPorts.SilentExternalPorts;
 using System.Threading.Tasks;
 
-namespace InteractionFlow.Standard.Externals.Silents
+namespace InteractionFlow.Standard.Externals.SilentExternals
 {
     public abstract class SilentSend<TArg>(params IFlowNode[] dependency) : SilentExternal(dependency), ISilentSendPort<TArg>
     {

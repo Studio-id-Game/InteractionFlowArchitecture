a057b8af9b7c3d5196b708fa4d2afb5e90369a16
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2026-07-10 18:05:17 +0900

    #59 NuGet.org 経由で最新版をインストールしてチェックする手動ワークフロー

---
+++ InteractionFlow.PackageInstallCheck/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace InteractionFlow.PackageInstallCheck
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

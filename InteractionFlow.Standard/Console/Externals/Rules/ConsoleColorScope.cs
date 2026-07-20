f7376e4d832d93117db9ee5dde65f0850470bf12
けもみみ愛してる <hirohiro.j3cub@gmail.com>
2026-07-20 17:29:06 +0900

    名前の単純化

--- InteractionFlow.Standard/Console/Externals/Rules/ConsoleColorScope.cs
+++ InteractionFlow.Standard/Console/Externals/Rules/ConsoleColorScope.cs
@@ -8,27 +8,27 @@ namespace InteractionFlow.Standard.Console.Externals.Rules
     /// </summary>
     public class ConsoleColorScope : IDisposable
     {
-        private readonly ConsoleColor foregroundColor = global::System.Console.ForegroundColor;
-        private readonly ConsoleColor backgroundColor = global::System.Console.BackgroundColor;
+        private readonly ConsoleColor foregroundColor = System.Console.ForegroundColor;
+        private readonly ConsoleColor backgroundColor = System.Console.BackgroundColor;
 
         /// <summary>
         /// 現在のコンソール色を取得または設定します。
         /// </summary>
         public ConsoleColorSet State
         {
-            get => new(global::System.Console.ForegroundColor, global::System.Console.BackgroundColor);
+            get => new(System.Console.ForegroundColor, System.Console.BackgroundColor);
             set
             {
-                global::System.Console.ForegroundColor = value.Foreground;
-                global::System.Console.BackgroundColor = value.Background;
+                System.Console.ForegroundColor = value.Foreground;
+                System.Console.BackgroundColor = value.Background;
             }
         }
 
         /// <inheritdoc/>
         public void Dispose()
         {
-            global::System.Console.ForegroundColor = foregroundColor;
-            global::System.Console.BackgroundColor = backgroundColor;
+            System.Console.ForegroundColor = foregroundColor;
+            System.Console.BackgroundColor = backgroundColor;
         }
     }
 }
